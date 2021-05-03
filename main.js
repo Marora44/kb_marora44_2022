bc26740e1cb38bb10e669cd2a6401929aa18e0a2
Marora44 <83252659+Marora44@users.noreply.github.com>
2021-05-03 14:56:44 -0400

    updated for loop and created while loop
    cArgs[i] is no longer set to an empty string in line 7, instead it is set to the phonetic equivalent of the rightmost digit of currentNumber
    in line 8 the rightmost digit of currentNumber is truncated
    
    the while loop in line 9 performs the same actions as lines 7 and 8 except it prepends the phonetic equivalent of the current rightmost digit of currentNumber (if any are left) to cArgs[i]

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ var phonetics = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight",
 var i;
 for (i = 0; i < cArgs.length; i++){
   var currentNumber = parseInt(cArgs[i]);
-  cArgs[i] = "";
+  cArgs[i] = phonetics[currentNumber%10];
+  currentNumber = Math.floor(currentNumber/10);
+  while (currentNumber > 0){
+    cArgs[i] = phonetics[currentNumber%10] + cArgs[i];
+    currentNumber = Math.floor(currentNumber/10);
+  }
 }
