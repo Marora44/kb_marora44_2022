bcce97bb5456541657bfa7d64060d51379ec7cff
Marora44 <83252659+Marora44@users.noreply.github.com>
2021-05-03 14:32:11 -0400

    set up for loop
    created a for loop to parse command line inputs
    variable currentNumber is used to store the integer representation of each number from the command line arguments
    the string representation of the number in cArgs is changed to an empty string which will eventually contain the phonetic representation of the number

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 var cArgs = process.argv.slice(2);
 var phonetics = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+
+var i;
+for (i = 0; i < cArgs.length; i++){
+  var currentNumber = parseInt(cArgs[i]);
+  cArgs[i] = "";
+}
